f2fd4a046fe6bfa4a8c4942572f1559978bdd4fb
dp1008 <38103879+dp1008@users.noreply.github.com>
2022-09-21 10:38:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "n-virginia.pem"
+  default     = "n-virginia"
 }
 
 variable "instance_type" {
